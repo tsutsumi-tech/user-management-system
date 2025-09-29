179062d3d71f8e5de3578db4eb5d8d85f41a081e
tsutsumi-tech <ayaka.tsutsumi@fox-hound.jp>
2025-09-29 17:13:53 +0900

    タスク1‐3　完了

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -9,13 +9,14 @@ import Link from "next/link";
 import React from "react";
 import { User } from "../types/User";
 import DeleteUserButton from "./DeleteUserButton";
+import CustomButton from "./parts/CustomButton";
 
 interface UserCardProps {
   user: User;
   onDelete: (userId: number) => void;
 }
 
-const UserCard: React.FC<UserCardProps> = ({ user,onDelete }) => {
+const UserCard: React.FC<UserCardProps> = ({ user, onDelete }) => {
   return (
     <Card sx={{ minWidth: 275, mb: 2 }}>
       <CardContent>
@@ -26,13 +27,19 @@ const UserCard: React.FC<UserCardProps> = ({ user,onDelete }) => {
         <Typography variant="body2">役割: {user.role}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" component={Link} href={`/users/${user.id}/details`}>
+        <Button
+          size="small"
+          component={Link}
+          href={`/users/${user.id}/details`}
+        >
           詳細
         </Button>
         <Button size="small" component={Link} href={`/users/${user.id}/edit`}>
           編集
         </Button>
-        <DeleteUserButton userId={user.id} onDelete={onDelete} />
+        <CustomButton onClick={() => onDelete(user.id)} variantType="danger">
+          削除
+        </CustomButton>
       </CardActions>
     </Card>
   );
--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UserCard from "./UserCard";
 import { User } from "@/types/User";
+import { softDeleteUser } from "@/utils/api";
 
 interface UserListProps {
   users: User[];
@@ -14,10 +15,20 @@ const UserList: React.FC<UserListProps> = ({ users }) => {
       users.filter((user: User) => user.id !== deletedUserId)
     );
   };
+  const handleDelete = async (userId: number) => {
+    if (confirm("本当にこのユーザーを削除しますか？")) {
+      try {
+        await softDeleteUser(userId);
+        handleUserDeleted(userId);
+      } catch (error) {
+        alert("削除に失敗しました。" + error);
+      }
+    }
+  };
   return (
     <div>
       {filterUsers.map((user) => (
-        <UserCard key={user.id} user={user} onDelete={handleUserDeleted}/>
+        <UserCard key={user.id} user={user} onDelete={handleDelete} />
       ))}
     </div>
   );
