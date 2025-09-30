9fe763b9ed282acb86f2e97434e5c5823f1a21f0
tsutsumi-tech <ayaka.tsutsumi@fox-hound.jp>
2025-09-30 18:19:39 +0900

    タスク3－3　完了

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -6,6 +6,7 @@ import CustomCard from "./parts/CustomCard";
 import { CardActions, Button } from "@mui/material";
 import Link from "next/link";
 import CustomButton from "./parts/CustomButton";
+import CustomModal from "./parts/CustomModal";
 
 interface UserListProps {
   users: User[];
@@ -13,6 +14,7 @@ interface UserListProps {
 
 const UserList: React.FC<UserListProps> = ({ users }) => {
   const [filterUsers, setFilterUsers] = useState<User[]>(users);
+  const [open, setOpen] = useState(false);
 
   const handleUserDeleted = (deletedUserId: number) => {
     setFilterUsers((users) =>
@@ -20,47 +22,54 @@ const UserList: React.FC<UserListProps> = ({ users }) => {
     );
   };
   const handleDelete = async (userId: number) => {
-    if (confirm("本当にこのユーザーを削除しますか？")) {
-      try {
-        await softDeleteUser(userId);
-        handleUserDeleted(userId);
-      } catch (error) {
-        alert("削除に失敗しました。" + error);
-      }
+    try {
+      await softDeleteUser(userId);
+      handleUserDeleted(userId);
+    } catch (error) {
+      alert("削除に失敗しました。" + error);
     }
   };
   return (
     <div>
       {filterUsers.map((user) => (
-        <CustomCard
-          key={user.id}
-          title={user.name}
-          description={`メール: ${user.email} / 役割: ${user.role}`}
-          actions={
-            <CardActions>
-              <Button
-                size="small"
-                component={Link}
-                href={`/users/${user.id}/details`}
-              >
-                詳細
-              </Button>
-              <Button
-                size="small"
-                component={Link}
-                href={`/users/${user.id}/edit`}
-              >
-                編集
-              </Button>
-              <CustomButton
-                onClick={() => handleDelete(user.id)}
-                variantType="danger"
-              >
-                削除
-              </CustomButton>
-            </CardActions>
-          }
-        />
+        <>
+          <CustomCard
+            key={user.id}
+            title={user.name}
+            description={`メール: ${user.email} / 役割: ${user.role}`}
+            actions={
+              <CardActions>
+                <Button
+                  size="small"
+                  component={Link}
+                  href={`/users/${user.id}/details`}
+                >
+                  詳細
+                </Button>
+                <Button
+                  size="small"
+                  component={Link}
+                  href={`/users/${user.id}/edit`}
+                >
+                  編集
+                </Button>
+                <CustomButton onClick={() => setOpen(true)} variantType="danger">
+                  削除
+                </CustomButton>
+              </CardActions>
+            }
+          />
+          <CustomModal
+            open={open}
+            title="ユーザー削除確認"
+            content="このユーザーを削除してもよろしいですか？"
+            // onCloceはsetOpenにfalseを渡す
+            onClose={() => setOpen(false)}
+            // onConfirmはalert()を使ってクリックしたことを知らせて
+            // setOpenにfalseを渡す
+            onConfirm={() => handleDelete(user.id)}
+          />
+        </>
       ))}
     </div>
   );
